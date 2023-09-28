177dc49bd7a629d9c935cef461a1dc30fa194903
ddawood <ddawood@udel.edu>
2023-09-27 20:45:45 -0400

    merge conflict errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
-import { Button, Col, Container, Row } from "react-bootstrap";
-=======
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
->>>>>>> upstream/task-state
 
 function App(): JSX.Element {
     return (
@@ -17,57 +13,6 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-<<<<<<< HEAD
-            <h1>Edited By: Dina Dawood</h1>
-            <p>
-                Hello World! Edit <code>src/App.tsx</code> and save. This page
-                will automatically reload.
-            </p>
-            <img
-                src="https://iibawards-prod.s3.amazonaws.com/projects/images/000/002/333/large.png?1505504208"
-                alt="A picture of Computer Science Concepts"
-            />
-            <ol>
-                <li>First item</li>
-                <li>Second item</li>
-                <li>Third item</li>
-            </ol>
-            <Button onClick={() => console.log("Hello World!")}>
-                Log Hello World
-            </Button>
-            <Container>
-                <Row>
-                    <Col>
-                        Column 1 of 2
-                        <div
-                            style={{
-                                width: "10px",
-                                height: "5px",
-                                backgroundColor: "red"
-                            }}
-                        >
-                            this will place a red-filled rectangle in each
-                            column
-                        </div>
-                    </Col>
-                    <Col>
-                        Column 2 of 2. You can put whatever you want in here,
-                        and it will be on the right side. Maybe try adding an
-                        image?
-                        <div
-                            style={{
-                                width: "10px",
-                                height: "5px",
-                                backgroundColor: "red"
-                            }}
-                        >
-                            this will place a red-filled rectangle in each
-                            column
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-=======
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -80,7 +25,6 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-state
         </div>
     );
 }
