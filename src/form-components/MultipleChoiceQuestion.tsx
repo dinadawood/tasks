c4d2e22a123f818b02c6f139a36b935d739768a8
ddawood <ddawood@udel.edu>
2023-12-10 17:13:03 -0500

    passed multiplechoicequestion

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,26 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>(options[0]);
+
+    function updatingAnswer(event: React.ChangeEvent<HTMLSelectElement>) {
+        setAnswer(event.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group>
+                <Form.Label>Select an Option:</Form.Label>
+                <Form.Select value={answer} onChange={updatingAnswer}>
+                    {options.map((o: string) => (
+                        <option key={o} value={o}>
+                            {o}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <div>{answer === expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
