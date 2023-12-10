2dc54cb4146d2d225e78b0614fc54ea8dedaa4d5
ddawood <ddawood@udel.edu>
2023-12-10 17:12:02 -0500

    passed doublehalf.tsx

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -28,7 +28,10 @@ export function DoubleHalf(): JSX.Element {
         <div>
             <h3>Double Half</h3>
             <div>
-                The current value is: <span>{dhValue}</span>
+                The current value is:
+                <span>
+                    <b> {dhValue}</b>
+                </span>
             </div>
             <Doubler dhValue={dhValue} setDhValue={setDhValue}></Doubler>
             <Halver dhValue={dhValue} setDhValue={setDhValue}></Halver>
