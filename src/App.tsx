30ce878e1c15b3a34c626400921a5fd17e861b4f
ddawood <ddawood@udel.edu>
2023-12-10 21:52:13 -0500

    quizzer changes

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ import { GiveAttempts } from "./form-components/GiveAttempts";
 import { EditMode } from "./form-components/EditMode";
 import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
 import { ChangeColor } from "./form-components/ChangeColor";
-import { Quizzer } from "./quizzer/Quizzer";
+import { Quizzer } from "./quizzer/QuizzerComponents/Quizzer";
 
 function App(): JSX.Element {
     return (
