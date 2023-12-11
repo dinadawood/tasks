26cb89120a35c45dceac8d5c12e45371eaaf30fd
ddawood <ddawood@udel.edu>
2023-12-10 23:56:11 -0500

    debugged errors in addquizmodal.tsx

--- src/quizzer/QuizzerComponents/AddQuizModal.tsx
+++ src/quizzer/QuizzerComponents/AddQuizModal.tsx
@@ -1,8 +1,17 @@
 import React, { useState } from "react";
 import { Button, Modal, Form } from "react-bootstrap";
 
-export const AddQuizModal = ({ show, handleClose, addQuiz }: {}) => {
+export const AddQuizModal = ({
+    show,
+    handleClose,
+    addQuiz
+}: {
+    show: boolean;
+    handleClose: () => void;
+    addQuiz: (title: string, body: string) => void;
+}) => {
     const [title, setTitle] = useState<string>("Example Quiz");
+    const [body, setBody] = useState<string>("Example Description");
 
     const saveChanges = () => {
         addQuiz(title, body);
@@ -31,7 +40,9 @@ export const AddQuizModal = ({ show, handleClose, addQuiz }: {}) => {
                             as="textarea"
                             rows={3}
                             value={body}
-                            onChange={}
+                            onChange={(
+                                e: React.ChangeEvent<HTMLInputElement>
+                            ) => setTitle(e.target.value)}
                         ></Form.Control>
                     </Form.Group>
                 </Modal.Body>
